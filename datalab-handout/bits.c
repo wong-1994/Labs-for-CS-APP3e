4e63ca1ffe68257cd28edae68652c2faac5ae3a3
wong-1994 <seanwxj@tongji.edu.cn>
2023-03-27 17:33:04 +0800

    data-lab to rating2

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -143,7 +143,9 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  int bothOnes = (x & y);
+  int bothZeros = ((~x) & (~y));
+  return ((~bothOnes) & (~bothZeros));
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -152,9 +154,7 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-
-  return 2;
-
+  return (1 << 31);
 }
 //2
 /*
@@ -165,7 +165,9 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+  int allOnes = !!(x + 1);
+  int plusOneEqualNot = !(~((x + 1) ^ x));
+  return plusOneEqualNot & allOnes;
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -176,7 +178,9 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+  int divide1 = ((x >> 16) & x);
+  int divide2 = ((divide1 >> 8) & divide1);
+  return !((divide2 & 0xAA) ^ 0xAA);
 }
 /* 
  * negate - return -x 
@@ -186,7 +190,7 @@ int allOddBits(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+  return (~x + 1);
 }
 //3
 /* 
